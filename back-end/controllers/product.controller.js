5f5c2c9b626b4c31fd8b46d71fbdbb25d10fdc44
kidusshoa <captainkevin2008@gmail.com>
2024-09-08 14:44:32 +0300

    products api full

---
+++ back-end/controllers/product.controller.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import Product from "../models/product.model.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching products", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (error) {
+    console.log("Error in creating product", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.log("Error in deleting product", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(201)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleting product", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
--- back-end/index.js
+++ back-end/index.js
@@ -1,60 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
 
-const app = express();
-
-app.use(express.json());
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({ success: true, data: products });
-  } catch (error) {
-    console.log("Error in fetching products", error);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(404)
-      .json({ success: false, message: "Please provide all fields" });
-  }
+app.use(express.json());
 
-  const newProduct = new Product(product);
+app.use("/api/products", productRoutes);
 
-  try {
-    await newProduct.save();
-    res.status(201).json({
-      success: true,
-      data: newProduct,
-    });
-  } catch (error) {
-    console.log("Error in creating product", error);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    await Product.findByIdAndDelete(id);
-    res
-      .status(201)
-      .json({ success: true, message: "Product deleted successfully" });
-  } catch (error) {
-    console.log("Error in deleting product", error);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.listen(4000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("listening on port 4000");
+  console.log("Server started at http://localhost:" + PORT);
 });
